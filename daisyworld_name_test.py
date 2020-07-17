3ef64ea17847df3b08ef1557b2610f79409179d9
GideonC172P <68059751+GideonC172P@users.noreply.github.com>
2020-07-17 20:32:04 +0200

    Add files via upload

---
+++ daisyworld_name_test.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 20 19:19:16 2019
+
+@author: tammas loughran
+@email: t.loughran@lmu.de
+
+This is an implementation of daisy world: a theoretical 
+model of the Gaia hypothesis. This theory was initially developed by 
+Lovelock (1983) to demonstrate the plausibility of living things 
+interacting with, and regulating, their environment.
+
+    Wood, A. J., G. J. Ackland, J. G. Dyke, H. T. P. Williams, and T. M. 
+        Lenton, 2015: Daisywolrd: a Review. Rev. Geophys., 46, RG1001, 
+        https://doi.org/10.1029/2006RG000217.
+
+    Copyright (C) 2019  Tammas Loughran
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+__name__=='__not_main__'
+
+if __name__=='__main__':
+    print('execute main program')
+    def albedo(alphaw, alphab, alphag, aw, ab, ag):
+        return alphaw*aw + alphab*ab + alphag*ag
+
+
+    def daisy_replicator(alpha, alphag, beta, gamma):
+        return alpha*(alphag*beta-gamma)
+
+
+    def beta(temp, optimum=295.65, tmin=278.15, tmax=313.15, k=0.003265):
+
+        if (temp>tmin)&(temp<tmax):
+            return 1 - k*(temp-optimum)**2
+        else:
+            return 0
+
+
+    def euler(initial, tendency, h=1):
+
+        return initial + h*tendency
+
+
+    def local_temp(A, albedo, T, q=30):
+
+        return q*(A-albedo)+T
+
+
+    def planetary_temp(S, A, L=1.0):
+    
+        sigma = 5.67032e-8 # Stephan-Bolzmann constant.
+        return ((S*L*(1-A))/sigma)**(1/4.)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+__name__=='__main__'
+
+if __name__=='__main__':
+    print('execute main program')
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+import all_functions
+
+
+# Define constants and variables
+alphaw = 0.00  # Cover fraction of white daisies
+alphab = 0.00  # Cover fraction of black daisies
+p = 1           # The fraction of habitable surface
+alphag = p-alphaw-alphab # Cover fraction of bare ground
+aw = 0.75       # Albedo of white daisies
+ab = 0.25       # Albedo of black daisies
+ag = 0.5        # Albedo of bare ground
+gamma = 0.3     # The death rate 
+S = 1000        # Solar constant (W/m^2)
+maxn = 1000     # Maximum number of iterations
+tol = 0.000001  # Tollerance of solution
+luminosities = np.arange(0.5,1.6, 0.002) # Stelar luminosities
+alphaw_out = np.ones(len(luminosities))*np.nan # Output variable for white
+alphab_out = np.ones(len(luminosities))*np.nan # Output variable for black
+temp_out = np.ones(len(luminosities))*np.nan   # Output variable for temp
+
+# Main loop for changing luminosity
+for i,L in enumerate(luminosities):
+    # Set a minimum for cover fractions
+    if alphaw<0.01: alphaw = 0.00
+    if alphab<0.01: alphab = 0.00
+    alphag = p-alphaw-alphab
+    # Reset counters
+    n = 0
+    changew, changeb = 1,1
+    # Run loop for daisy earth.
+    while (n<maxn) and (changew>tol) and (changeb>tol):
+        # Store the initial cover fractions
+        sw,sb = alphaw, alphab
+        # Planetary albedo
+        planet_albedo = all_functions.albedo(alphaw,alphab,alphag,aw,ab,ag)
+        # Planetary temperature
+        T = all_functions.planetary_temp(S,planet_albedo, L=L)
+        # Local temperature
+        Tw = all_functions.local_temp(planet_albedo,aw,T)
+        Tb = all_functions.local_temp(planet_albedo,ab,T)
+        # Birth rate
+        betaw = all_functions.beta(Tw)
+        betab = all_functions.beta(Tb)
+        # Change in daisies
+        dawdt = all_functions.daisy_replicator(alphaw, alphag, betaw, gamma)
+        dabdt = all_functions.daisy_replicator(alphab, alphag, betab, gamma)
+        # Integrate
+        alphaw = all_functions.euler(alphaw, dawdt)
+        alphab =all_functions. euler(alphab, dabdt)
+        alphag = p-alphaw-alphab
+        n += 1
+    # Store the output
+    alphaw_out[i] = alphaw
+    alphab_out[i] = alphab
+    temp_out[i] = T
+
+# Plot the results
+# Cover fractions
+white = plt.plot(luminosities,alphaw_out*100,'b', label='White')
+black = plt.plot(luminosities,alphab_out*100,'k', label='Black')
+plt.legend(loc='upper right')
+plt.xlabel('Luminosity')
+plt.ylabel('Surface cover %')
+plt.title('Cover fractions')
+plt.show()
+
+# Planetary temperature
+plt.figure()
+plt.plot(luminosities,temp_out-273.15,'r')
+plt.xlabel('Luminosity')
+plt.ylabel('Temperature (C)')
+plt.title('Planetary temperature')
+plt.show()
